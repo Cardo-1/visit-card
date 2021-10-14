dbcd2e1704a1f11681f7f9351179e49592aee335
aydisaddem <sdouma91@gmail.com>
2021-10-14 10:57:12 +0100

    main page

---
+++ main.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    $('.pic').hide()
+    $('.picBigC').hide()
+    $('.picBigP').hide()
+ 
+
+    $('#company').click(function(){
+        $('#personal').hide()
+        $('#company').hide()
+        $('#companyTxt').hide()
+        $('#personalTxt').hide()
+        $('.picBigC').show()
+    })
+
+    $('#personal').click(function(){
+        $('#personal').hide()
+        $('#company').hide()
+        $('#companyTxt').hide()
+        $('#personalTxt').hide()
+        $('.picBigP').show()
+     })
+
+    $('#pic1Big').click(function(){
+        $('#personal').hide()
+        $('#company').hide()
+        $('.picBigC').hide()
+        $('#pic1').show()
+    })
+
+    $('#pic2Big').click(function(){
+        $('#personal').hide()
+        $('#company').hide()
+        $('.picBigC').hide()
+        $('#pic2').show()
+    })
+
+    $('#pic3Big').click(function(){
+        $('#personal').hide()
+        $('#company').hide()
+        $('.picBigC').hide()
+        $('#pic3').show()
+    })
+
+    $('#pic4Big').click(function(){
+        $('#personal').hide()
+        $('#company').hide()
+        $('.picBigC').hide()
+        $('#pic4').show()
+    })
+
+    $('#pic5Big').click(function(){
+        $('#personal').hide()
+        $('#company').hide()
+        $('.picBigP').hide()
+        $('#pic5').show()
+    })
+
+    $('#pic6Big').click(function(){
+        $('#personal').hide()
+        $('#company').hide()
+        $('.picBigP').hide()
+        $('#pic6').show()
+    })
+
+    $('#pic7Big').click(function(){
+        $('#personal').hide()
+        $('#company').hide()
+        $('.picBigP').hide()
+        $('#pic7').show()
+    })
+
+    $('#pic8Big').click(function(){
+        $('#personal').hide()
+        $('#company').hide()
+        $('.picBigP').hide()
+        $('#pic8').show()
+    })
+
+    $('#tryItC').click(function(){
+        if ($( "Serif:selected" )){
+            
+        }
+
+
+})
